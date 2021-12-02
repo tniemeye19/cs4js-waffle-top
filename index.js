fa11cc40bf333f2ad6f9f2a6ed2cc82ebc864902
tniemeye19 <timothy.niemeyer19@gmail.com>
2021-12-01 19:32:54 -0500

    created benchmark tests for linear search and binary search algorithms

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 const Benchmark = require('benchmark');
+const { linearSeach, binarySearch } = require('./search');
+
+const numbers = [];
+for (let i = 1; i <= 4000000; i++) {
+    // populate the array with the numbers 1 to 1,000,000
+    numbers.push(i)
+}
+
+// grab the last number in the array as the number we want to find
+const target = numbers[0];
 
 const suite = new Benchmark.Suite;
 
 suite
-    .add('random test', function() {
-        // benchmark generating a random number
-        Math.floor(Math.random() * 10) + 1;
-    })
-    .on('complete', function() {
-        console.log(this);
-    })
-    .run();
+  .add('linear search', function() {
+    linearSearch(numbers, target);
+  })
+  .add('binary search', function() {
+      binarySearch(numbers, target, 0, numbers.length-1)
+  })
+  .on('complete', function() {
+    // loop over and print each result
+    this.forEach(result => {
+        console.log(`${result.name} averaged ${result.stats.mean*1000} milliseconds.`)
+    });
+  })
+  .run();
